15b8a1b2d535dc2150a8c02fe5a49bd2cb649a28
karthikeyan <karthikeyan@embeegroup-eg.local>
2026-07-22 15:00:21 +0300

    Add valid Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Database setup-kaga
+import { getFirestore } from "firebase/firestore";
 
-// Neenga copy panna config-ah inga paste pannunga
 const firebaseConfig = {
   apiKey: "AIzaSyDZNZP_gVBXZs1xDvKKdfVZ7SWfacvM-5A",
   authDomain: "smv-insight.firebaseapp.com",
@@ -9,11 +8,8 @@ const firebaseConfig = {
   storageBucket: "smv-insight.firebasestorage.app",
   messagingSenderId: "290153757190",
   appId: "1:290153757190:web:8ad595d9a20e001135a187",
-  measurementId: "G-F1HMSCJXBH"
+  measurementId: "G-F1HM5CJX0H"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Database-ah export pannunga
 export const db = getFirestore(app);
